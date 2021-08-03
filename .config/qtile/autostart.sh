dc20c728a736e1090f4c75e0df404b91583353b3
centaurialpha <acostadariogabriel@gmail.com>
2021-08-03 00:02:15 -0300

    goku wallpaper je
    Signed-off-by: centaurialpha <acostadariogabriel@gmail.com>

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-feh --bg-fill ~/Pictures/wallpapers/winter-city.jpg &
+feh --bg-fill ~/Pictures/wallpapers/goku-ui-1.jpg &
 picom --experimental-backends &
 dunst -config ~/.config/dunst/dunstrc &
 # setxkbmap -layout us -variant altgr-intl -option nodeadkeys &
