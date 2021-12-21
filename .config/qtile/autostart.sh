ad7d6ec58b09b25ee3bd41a20f98b1171763adb9
Gabriel Acosta <acostadariogabriel@gmail.com>
2021-12-21 09:08:34 -0300

    udpate configs

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-feh --bg-fill ~/Pictures/wallpapers/man-telescope.jpg &
+feh --bg-fill ~/Pictures/wallpapers/interstellar.jpg &
 picom --experimental-backends &
 dunst -config ~/.config/dunst/dunstrc &
