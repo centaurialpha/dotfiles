09d981469cc2d9d43fea2a0ebe010edd5d2bb934
Gabriel Acosta <acostadariogabriel@gmail.com>
2023-01-05 02:14:54 -0300

    update configs

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-feh --bg-fill ~/Pictures/wallpapers/wp4098405-jupiter-wallpapers.jpg &
-picom --experimental-backends &
+feh --bg-fill ~/Pictures/wallpapers/minimalist-blackhole.jpg &
+picom &
 dunst -config ~/.config/dunst/dunstrc &
